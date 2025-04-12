94e9c03c88e9037a7b1e50b36ef94e8105363c41
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-04-12 15:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+
+window.onload = function() {
+  const button = document.querySelector('input[type="button"]');
+  const colorSelect = document.getElementById("colorSelect");
+
+  button.addEventListener("click", function() {
+    // Remove the selected option
+    colorSelect.remove(colorSelect.selectedIndex);
+  });
+};
